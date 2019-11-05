e7263f9285e8d4989a75323e039310c89f01c0a5
Brandy Mello <brandyamello@gmail.com>
2019-11-04 17:34:32 -0700

    Add the component and intitial structure for the gallery

---
+++ gallery_game/src/components/GalleryPiece.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Gallery Piece</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "GalleryPiece"
+}
+</script>
+<style scoped>
+  .gallery-piece {
+    border: 2px solid black;
+  }
+</style>
