b6e18b5e8b9cfea5747bbb41ed2c322b714c30ad
Brandy Mello <brandyamello@gmail.com>
2019-11-04 20:48:37 -0700

    Reorganized files and got api call working

--- gallery_game/src/App.vue
+++ gallery_game/src/App.vue
@@ -1,41 +1,71 @@
 <template>
-  <div id="app">
-    <Pieces :pieces="pieces"/>
-    <!-- {{pieces[0].title}} -->
-  </div>
+  <main id="app">
+    <!-- <GalleryPiece v-bind:pieces="pieces"/> -->
+    <article :key="piece.id" v-for="piece in pieces">
+      <GalleryPiece :piece="piece"/>
+    </article>
+  </main>
 </template>
 
 <script>
-import Pieces from './components/Pieces'
+// import { getArt } from '../src/apicalls/apicalls';
+import GalleryPiece from './components/GalleryPiece';
+// const givemesomething = getArt();
+// console.log(givemesomething)
 
 export default {
   name: 'app',
   components: {
-    Pieces
+    GalleryPiece
   },
   data() {
     return {
       pieces: [
-        {
-          id: 1,
-          title: "Title1",
-          image: "url to image1",
-        },
-        {
-          id: 2,
-          title: "Title2",
-          image: "url to image2",
-        },
-        {
-          id: 1,
-          title: "Title2",
-          image: "url to image3",
+        // {
+        //   id: 1,
+        //   title: "Title1",
+        //   image: "url to image1",
+        // },
+        // {
+        //   id: 2,
+        //   title: "Title2",
+        //   image: "url to image2",
+        // },
+        // {
+        //   id: 3,
+        //   title: "Title3",
+        //   image: "url to image3",
+        // }
+      ],
+      error: "try again"
+    }
+  },
+  mounted() {
+    this.getArt();
+  },
+  methods: {
+      async getArt() {
+        try {
+          const response = await fetch('https://api.harvardartmuseums.org/object?gallerynumber=1220&size=21&apikey=36e7a220-fdc7-11e9-9a5d-0b0eede4b42a');
+
+          const artWorkInfo = await response.json();
+          this.arrangeData(artWorkInfo);
+        } catch (error) {
+          this.error = { error }
         }
-      ]
+    },
+    arrangeData(artData) {
+      // const id = 0;
+      this.pieces = artData.records.map(piece => {
+         return {
+          //  id: id += 1,
+           title: piece.title,
+           image: piece.primaryimageurl
+         }
+      });
     }
   }
 }
-
 
 </script>
 
@@ -43,7 +73,7 @@ export default {
   * {
     box-sizing: border-box;
     margin: 0;
-    padding: 0;
+    padding: 10px;
   }
 
   body {
--- gallery_game/src/components/GalleryPiece.vue
+++ gallery_game/src/components/GalleryPiece.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-    <p>Gallery Piece</p>
+  <div class="gallery-piece">
+    <p>{{ piece.title }}</p>
+    <p>{{ piece.image }}</p>
   </div>
 </template>
 <script>
 export default {
-  name: "GalleryPiece"
+  name: "GalleryPiece",
+  props: ["piece"]
 }
 </script>
 <style scoped>
   .gallery-piece {
     border: 2px solid black;
+    /* background-image: {piece.image}; */
   }
 </style>
