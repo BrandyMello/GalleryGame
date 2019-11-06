227e0c4d4e67d35b552d4ed2fbc7aab5d0b62b64
Brandy Mello <brandyamello@gmail.com>
2019-11-06 10:31:38 -0700

    Attempt to turn into clickable cards failed

--- gallery_game/src/components/GalleryPiece.vue
+++ gallery_game/src/components/GalleryPiece.vue
@@ -1,19 +1,33 @@
 <template>
+<div class="cards">
   <div class="gallery-piece">
-    <p class="title">{{ piece.title }}</p>
-    <img :src="piece.image" />
-    <p class="subtitle">{{ piece.date }}</p>
-    <p class="subtitle">{{ piece.artist }}</p>
+      <p class="title">{{ piece.title }}</p>
+      <img :src="piece.image" v-show="flipCard"/>
+      <p class="subtitle">{{ piece.date }}</p>
+      <p class="subtitle">{{ piece.artist }}</p> 
   </div>
+  <!-- <div  v-on:click="flipCard" class="blank-card">
+  </div> -->
+</div>
 </template>
 <script>
 export default {
   name: "GalleryPiece",
-  props: ["piece"]
+  props: ["piece"],
+  data() {
+    return
+  },
+  methods : {
+    flipCard() {
+      this.cardFlipped = !this.cardFlipped
+    }
+  }
 }
 </script>
 <style scoped>
   .gallery-piece {
+    background-color: #37361C;
+    color: #f8f0dd;
     border: 2px solid black;
     width: 230px;
     height: 370px;
@@ -36,5 +50,18 @@ export default {
     font-style: italic;
     padding: 0;
     margin-left: 6%;
+  }
+
+  .blank-card {
+    position: relative;
+    bottom: 370px;
+    background-color: black;
+    border: 2px solid black;
+    width: 230px;
+    height: 370px;
+  }
+
+  .cards {
+    height: 370px;
   }
 </style>
